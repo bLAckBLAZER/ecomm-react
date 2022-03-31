9a68e7acfab13eb81077a80820bc67e3ae84514a
Omkar Jadhav <48726860+bLAckBLAZER@users.noreply.github.com>
2022-03-31 15:58:41 +0530

    [refactor] removed comment

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -4,7 +4,5 @@ import { Login } from "../../pages/Authentication/Login";
 export const PrivateRoute = ({ children }) => {
   const { authState } = useAuth();
 
-  console.log(authState);
-
   return authState.token ? children : <Login />;
 };
