9c432948911cbdf1bb1cd20f956fdcff84940e81
Omkar Jadhav <48726860+bLAckBLAZER@users.noreply.github.com>
2022-04-18 00:09:43 +0530

    added Modal dark background

--- src/components/NavBar/SideBar.js
+++ src/components/NavBar/SideBar.js
@@ -11,28 +11,30 @@ export const SideBar = ({
   userWishlist,
 }) => {
   return (
-    <aside className="mob-aside">
-      <div className="side-bar-item">
-        <i className="fas fa-times" onClick={() => setShowSideBar(false)}></i>
-      </div>
-      <div className="side-bar-item">
-        <Link to="/cart">My Cart</Link>
-      </div>
-      <div className="side-bar-item">
-        <Link to="/wishlist">My Wishlist</Link>
-      </div>
-      <div className="side-bar-item" style={{ justifySelf: "flex-end" }}>
-        {authState.token ? (
-          <Link
-            to="/logout"
-            onClick={() => userLogout(dispatchAuth, userDispatch)}
-          >
-            Logout
-          </Link>
-        ) : (
-          <Link to="/login">Login</Link>
-        )}
-      </div>
-    </aside>
+    <div className="modal-container">
+      <aside className="mob-aside">
+        <div className="side-bar-item">
+          <i className="fas fa-times" onClick={() => setShowSideBar(false)}></i>
+        </div>
+        <div className="side-bar-item">
+          <Link to="/cart">My Cart</Link>
+        </div>
+        <div className="side-bar-item">
+          <Link to="/wishlist">My Wishlist</Link>
+        </div>
+        <div className="side-bar-item" style={{ justifySelf: "flex-end" }}>
+          {authState.token ? (
+            <Link
+              to="/logout"
+              onClick={() => userLogout(dispatchAuth, userDispatch)}
+            >
+              Logout
+            </Link>
+          ) : (
+            <Link to="/login">Login</Link>
+          )}
+        </div>
+      </aside>
+    </div>
   );
 };
